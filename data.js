82d424a328e9f3961af89bbe370de153f0df41d6
IbarraentuMirada <jeniferchapuel1010@gmail.com>
2025-07-17 10:51:02 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,219 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-teatro-ex-colegio-teodoro-gmez",
+      "name": "Teatro Ex-Colegio Teodoro Gómez",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -0.6912404793500144,
+        "pitch": 0.09753525579019495,
+        "fov": 1.2988261968502064
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.056966236435212,
+          "pitch": 0.022820829381796415,
+          "title": "<br>",
+          "text": "Este escenario se utilizaba con fines artísticos, varias obras realizadas por estudiantes se exponian en esta sala."
+        },
+        {
+          "yaw": -0.01032811245855747,
+          "pitch": -0.0791428788297015,
+          "title": "Escenario",
+          "text": "En este escenario se realizaban obras teatrales realizadas por los estudiantes"
+        },
+        {
+          "yaw": 1.5330185252244846,
+          "pitch": -0.842401151610229,
+          "title": "Fotografías de Rectores",
+          "text": "Fotografías de los múltiples rectores que tuvo la institución&nbsp;"
+        },
+        {
+          "yaw": -1.8657814391649623,
+          "pitch": -0.698818777860458,
+          "title": "Fotografías de Rectores",
+          "text": "Fotografías de los múltiples rectores que tuvo la institución&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "1-sala-pinturas-ex-colegio-teodoro-gmez",
+      "name": "Sala Pinturas Ex-Colegio Teodoro Gómez",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.1265043331228899,
+        "pitch": -0.04001742074218839,
+        "fov": 1.2988261968502064
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.8753601528257793,
+          "pitch": -0.12040905160613491,
+          "title": "<br>",
+          "text": "Habitación que destaca por el decorado de sus paredes que se encuentra pintado"
+        },
+        {
+          "yaw": 1.1633160534205018,
+          "pitch": -0.8542842480545083,
+          "title": "Panoramas",
+          "text": "Dibujos de distintos paisajes y panoramas que decoraban la parte superior de las paredes"
+        },
+        {
+          "yaw": -0.011985617149116834,
+          "pitch": -0.092632624420105,
+          "title": "Restauración",
+          "text": "Estos muros se destruyeron durante el terremoto de Ibarra, pero fueron restaurados"
+        }
+      ]
+    },
+    {
+      "id": "2-saln-principal-casa-de-la-ibarreidad",
+      "name": "Salón Principal Casa de la Ibarreñidad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.23817694890565022,
+        "pitch": 0.1072412017613047,
+        "fov": 1.2988261968502064
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.4609094214779734,
+          "pitch": 0.0071091894349493145,
+          "title": "Balcón de Velazco Ibarra",
+          "text": "''Dadme un balcón y seré presidente''&nbsp;<br><br>Famoso balcón que Velazco Ibarra solía frecuentar para dar sus discursos hacia el parque central."
+        },
+        {
+          "yaw": 2.8087070997993617,
+          "pitch": 0.15254627695496126,
+          "title": "Heliodoro Ayala&nbsp;",
+          "text": "Primer dueño de la casa de la ibarreñidad"
+        },
+        {
+          "yaw": -2.7396871982148276,
+          "pitch": 0.13152466434869936,
+          "title": "Delfina Leoro",
+          "text": "Esposa de Heliodoro Ayala"
+        },
+        {
+          "yaw": -3.092846115416833,
+          "pitch": 0.09877484102863221,
+          "title": "Morelia",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.013331541497175081,
+          "pitch": 0.0169992721335408,
+          "title": "Familiares",
+          "text": "Familia Cercana de Heliodoro Ayala"
+        }
+      ]
+    },
+    {
+      "id": "3-saln-troya",
+      "name": "Salón Troya",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 2.5416317732283886,
+        "pitch": -0.04747900651392456,
+        "fov": 1.2988261968502064
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.9632929402869497,
+          "pitch": -0.07465421516744186,
+          "title": "<br>",
+          "text": "Ilustracion con un estilo realista representando la región amazonica y andina del Ecuador."
+        },
+        {
+          "yaw": 0.4821116608675169,
+          "pitch": -0.14606250346591665,
+          "title": "<br>",
+          "text": "Ilustración del Dr. Mariano Acosta, icono representante de la Iglesia, que ayudó en la reconstrucción de Ibarra"
+        },
+        {
+          "yaw": -0.719411345333759,
+          "pitch": -0.08247129341255466,
+          "title": "<br>",
+          "text": "Ilustración representativa del Terremoto de Ibarra. Suscitado el 16 de agosto de 1868."
+        },
+        {
+          "yaw": -3.0527490641141686,
+          "pitch": -0.11177710854216372,
+          "title": "<br>",
+          "text": "Pintura interpretativa de la fundación de Ibarra. 28 de Septiembre de 1606"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
